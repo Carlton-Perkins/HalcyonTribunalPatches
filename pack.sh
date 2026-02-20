1c2fec3b5455fcd0bf5519f5fe1f50e61d387f1c
Carlton-Perkins <github@carltonperkins.com>
2026-02-20 00:03:53 -0500

    fixed pack script to only include relevant files

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 VERSION=$(grep -o '"version": *"[^"]*"' modinfo.json | grep -o '[0-9][^"]*')
-zip -r "output/HalcyonTribunalPatches.${VERSION}.zip" . -x "pack.sh"
+zip -r "output/HalcyonTribunalPatches.${VERSION}.zip" . -x "pack.sh" -x "output/*" -x ".*" -x "*/.*"
